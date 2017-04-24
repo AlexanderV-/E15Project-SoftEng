b2263f8286d464694cf3af60aa0119ee96378f6e
AlexanderV-/E-15-Ingenieria-de-Software.git <Zero-G@hotmail.es>
2017-04-24 19:05:27 +0100

    Agregando ingreso activities.

--- app/src/main/java/juniorvalerav/e15project/Ingreso.java
+++ app/src/main/java/juniorvalerav/e15project/Ingreso.java
@@ -1,8 +1,8 @@
 package juniorvalerav.e15project;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
